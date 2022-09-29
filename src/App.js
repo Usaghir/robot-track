5aaffe9f70ffa2d97bb2a06ad7e882c5c51d05d1
usaghir <umer.saghir@live.com>
2022-09-29 16:56:45 +0200

    Add robot position ui and logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
+
+
 function App() {
+
+  const [moves, setMoves] = useState('');
+
+  const [position, setPosition] = useState('');
+
+  
+
+
+  const submitHandler = (e) => { 
+    e.preventDefault();
+    setPosition(moves);
+  }
+
+  const findPosition = (position) => { 
+    let leftMoves = 0;
+    let rightMoves = 0;
+    let forwardMoves = 0;
+
+  
+
+    for (let index = 0; index < position.length; index++) {
+      switch (position[index]) {
+        case 'L':
+
+          leftMoves++;
+          break;
+          case 'R':
+
+            rightMoves++;
+          break;
+          case 'F':
+
+            forwardMoves++;
+            break;
+      
+        default:
+          break;
+      }
+      
+    }
+ 
+  }
+
+  
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="vertical">
+       
+       <h2 className="active"> Robot-Track</h2>
+          <form onSubmit={submitHandler}>
+      <label>Initial Position
+        <input type="text" />
+          </label>
+          <label>Please enter your Steps:
+        <input type="text" required value={moves} onChange = {(e) => setMoves(e.target.value)} />
+          </label>
+          
+          <button type="submit">Submit</button>
+    </form>
+        
+      </div>
+      <label>Final Potion is
+        <div> {position}</div>
+          </label>
     </div>
   );
 }
