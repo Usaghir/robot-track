3f29977ffac21a4d8c110894c059b698400ff295
usaghir <umer.saghir@live.com>
2022-10-02 21:07:46 +0200

    Add robot position tracking features

--- src/App.js
+++ src/App.js
@@ -1,77 +1,141 @@
 import React, { useState } from 'react';
 import './App.css';
 
-
-
-
 function App() {
+  const [roomLength, setRoomLength] = useState(0);
+  const [roomWidth, setRoomWidth] = useState(0);
+
+  const [initialPositionX, setInitialPositionX] = useState(0);
+  const [initialPositionY, setInitialPositionY] = useState(0);
+  const [initialDirection, setInitialDirection] = useState('');
 
   const [moves, setMoves] = useState('');
+  const [finalPosition, setFinalPosition] = useState('');
 
-  const [position, setPosition] = useState('');
-
-  
-
-
-  const submitHandler = (e) => { 
+  const submitHandler = (e) => {
     e.preventDefault();
-    setPosition(moves);
-  }
+    findFinalPosition();
+  };
 
-  const findPosition = (position) => { 
-    let leftMoves = 0;
-    let rightMoves = 0;
-    let forwardMoves = 0;
+  const findFinalPosition = () => {
+    const cardinalDirections = 'WNES';
+    let positionX = initialPositionX;
+    let positionY = initialPositionY;
+    let direction = initialDirection;
 
-  
-
-    for (let index = 0; index < position.length; index++) {
-      switch (position[index]) {
+    for (let index = 0; index < moves.length; index++) {
+      switch (moves[index]) {
         case 'L':
-
-          leftMoves++;
+          if (direction === 'W') {
+            direction = 'S';
+          } else {
+            direction = cardinalDirections[cardinalDirections.indexOf(direction) - 1];
+          }
           break;
-          case 'R':
-
-            rightMoves++;
+        case 'R':
+          if (direction === 'S') {
+            direction = 'W';
+          } else {
+            direction = cardinalDirections[cardinalDirections.indexOf(direction) + 1];
+          }
           break;
-          case 'F':
-
-            forwardMoves++;
-            break;
-      
         default:
+          switch (direction) {
+            case 'E':
+              if (positionX < roomLength) {
+                positionX++;
+              }
+              break;
+            case 'W':
+              if (positionX > 0) {
+                positionX--;
+              }
+              break;
+            case 'N':
+              if (positionY < roomWidth) {
+                positionY++;
+              }
+              break;
+            default:
+              if (positionY > 0) {
+                positionY--;
+              }
+              break;
+          }
           break;
       }
-      
     }
- 
-  }
+    setFinalPosition(positionX +','+ positionY+','+direction);
+  };
 
-  
-  
-  
   return (
     <div className="App">
-      <div className="vertical">
-       
-       <h2 className="active"> Robot-Track</h2>
-          <form onSubmit={submitHandler}>
-      <label>Initial Position
-        <input type="text" />
+      <div className="container">
+        <h2 className="active center"> Robot Final Position Finder</h2>
+        <form onSubmit={submitHandler}>
+          <label>
+            Please provide what should be the length of the room 
+            <input
+              type="number"
+              min="1"
+              required
+              value={roomLength}
+              onChange={(e) => setRoomLength(e.target.value)}
+            />
           </label>
-          <label>Please enter your Steps:
-        <input type="text" required value={moves} onChange = {(e) => setMoves(e.target.value)} />
+          <label>
+          Please provide what should be the width of the room 
+            <input
+              type="number"
+              min="1"
+              required
+              value={roomWidth}
+              onChange={(e) => setRoomWidth(e.target.value)}
+            />
           </label>
-          
-          <button type="submit">Submit</button>
-    </form>
-        
+          <label>
+            Provide Robot's initial position in X axis
+            <input
+              type="text"
+              required
+              value={initialPositionX}
+              onChange={(e) => setInitialPositionX(e.target.value)}
+            />
+          </label>
+          <label>
+            Provide Robot's initial position in Y axis
+            <input
+              type="text"
+              required
+              value={initialPositionY}
+              onChange={(e) => setInitialPositionY(e.target.value)}
+            />
+          </label>
+          <label>
+            Provide Robot's Direction
+            <input
+              type="text"
+              required
+              value={initialDirection}
+              onChange={(e) => setInitialDirection(e.target.value)}
+            />
+          </label>
+          <label>
+            Add your moves
+            <input type="text" required value={moves} onChange={(e) => setMoves(e.target.value)} />
+          </label>
+<div className='center'>
+            <button  type="submit">Submit</button>
+            </div>
+        </form>
+   
+      <label >
+      <div ><h2 className = 'center'> Final Position of Robot is:
+            {'  ('+finalPosition+')'}</h2>
+            </div>
+      </label>
       </div>
-      <label>Final Potion is
-        <div> {position}</div>
-          </label>
-    </div>
+      </div>
   );
 }
 
